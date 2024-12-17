319264078ab3b1733469eb0c6c41ce250823fed3
Fede <fede.atzeni372@gmail.com>
2024-12-17 16:25:58 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // Chiedi distanza e età all'utente
 // Se l'età e maggiore di 65 applica 40% di sconto
-// Oppure l'età e minore di 20 applica 40% di sconto
+// Oppure l'età e minore di 18 applica 0% di sconto
 // Altrimenti nessuno sconto 
 // Calcola il prezzo in base alla tariffa, alla distanza e all'eventuale sconto
 // Restituisci il risultato 
@@ -33,8 +33,8 @@ else {
 // console.log(discount);
 
 // price
-let tot= tariff * distance
-price = (tot - (tot * discount)).toFixed(2)
+let tot= tariff * distance;
+price = (tot - (tot * discount)).toFixed(2);
 
 // output
 console.log("Il prezzo è di " + price + "€");
